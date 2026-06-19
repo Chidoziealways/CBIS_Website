4726aaadbc997422abad4e48c82782a49cfda487
Chidoziealways <uzowuluchidozie@gmail.com>
2026-06-19 16:20:33 +0100

    Proper Startt

---
+++ src/About.tsx
@@ -0,0 +1,11 @@
+﻿function About() {
+    return (
+        <>
+            <div className="about-div">
+                <p>This school... is one of the worst...</p>
+            </div>
+        </>
+    )
+}
+
+export default About
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { HashRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+//import {StrictMode} from "react"
 
 createRoot(document.getElementById('root')!).render(
   <HashRouter>
